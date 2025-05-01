391b77ca89376052a216f0921f846d2c84e1af0e
Santiago Arango <arangosanti05@gmail.com>
2025-04-30 21:01:07 -0500

    build aplication

---
+++ test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Santiago Arango Isaza on 5/08/24.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
